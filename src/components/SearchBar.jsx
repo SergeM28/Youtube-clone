62c3509040d3e9d4d58b12e39c971890020d2621
serge.muradian@gmail.com <serge.muradian@gmail.com>
2023-03-29 11:47:06 +0300

    Creating up searching functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -4,14 +4,26 @@ import { Paper, IconButton } from '@mui/material';
 import { Search } from '@mui/icons-material';
 
 const SearchBar = () => {
+
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(searchTerm) {
+      navigate(`/search/${searchTerm}`);
+      setSearchTerm('');
+    }
+  }
   return (
-    <Paper component='form' onSubmit={() => {}} 
+    <Paper component='form' onSubmit={handleSubmit} 
     sx={{borderRadius: 20, 
     border: '1px solid #e3e3e3', 
     pl: 2, boxShadow: 'none',
     mr: {sm: 5 }}}>
       <input className='search-bar' placeholder='Search'
-      value='' onChange={() => {}}/>
+      value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
       <IconButton>
         <Search type='submit' sx={{p: '10px', color: 'red'}}/>
       </IconButton>
