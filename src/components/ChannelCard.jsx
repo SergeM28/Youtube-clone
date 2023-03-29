811afebdac3a7f7ad473465f7ae43a702b2277ed
serge.muradian@gmail.com <serge.muradian@gmail.com>
2023-03-29 16:07:17 +0300

    Create VideoDetail component and add video playing functionality

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -4,26 +4,32 @@ import { Link } from 'react-router-dom';
 import { demoProfilePicture } from '../utils/constants';
 
 
-const ChannelCard = ({channelDetail, marginTop}) => {
+const ChannelCard = ({ channelDetail, marginTop }) => {
     return (
-        <Box sx={{boxShadow: 'none', borderRadius: '20px',
-        display: 'flex', justifyContent: 'center', alignItems: 'center',
-        width: {xs: '356px', md: '320px'}, height: '326px', margin: 'auto', marginTop}}>
+        <Box sx={{
+            boxShadow: 'none', borderRadius: '20px',
+            display: 'flex', justifyContent: 'center', alignItems: 'center',
+            width: { xs: '356px', md: '320px' }, height: '326px', margin: 'auto', marginTop
+        }}>
             <Link to={`/channel/${channelDetail?.id?.channelId}`}>
-                <CardContent sx={{display: 'flex', flexDirection: 'column', 
-            justifyContent: 'center', textAlign: 'center', color: '#fff'}}>
+                <CardContent sx={{
+                    display: 'flex', flexDirection: 'column',
+                    justifyContent: 'center', textAlign: 'center', color: '#fff'
+                }}>
                     <CardMedia image={channelDetail?.snippet?.thumbnails?.high?.url ||
-                    demoProfilePicture} alt={channelDetail?.snippet?.title}
-                    sx={{borderRadius: '50%', height: '180px', width: '180px', mb: 2,
-                    border: '1px solid #e3e3e3'}}/>
+                        demoProfilePicture} alt={channelDetail?.snippet?.title}
+                        sx={{
+                            borderRadius: '50%', height: '180px', width: '180px', mb: 2,
+                            border: '1px solid #e3e3e3'
+                        }} />
                     <Typography variant='h6'>
                         {channelDetail?.snippet?.title}
-                        <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px'}}/>
+                        <CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px' }} />
                     </Typography>
                     {channelDetail?.statistics?.subscriberCount && (
-                        <Typography>
+                        <Typography sx={{fontSize: '15px', fontWeight: 500, color: 'gray'}}>
                             {parseInt(channelDetail?.statistics?.subscriberCount).
-                            toLocaleString()} Subscribers
+                                toLocaleString()} Subscribers
                         </Typography>
                     )}
                 </CardContent>
